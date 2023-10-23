5eb16e37ee4003f75e79384a2cc74e9b498e0df9
cervs257 <cervantes257@gmail.com>
2023-10-22 21:12:41 -0400

    added time to main

--- python_yahoo_finance/main.py
+++ python_yahoo_finance/main.py
@@ -4,11 +4,15 @@ A script to create a command-line tool using python fire to get stock data from 
 
 import fire
 from lib import get_prices
+import time
 
 
 def main(ticker="AAPL", period="1mo", interval="1d"):
+    start_time = time.time()
     data = get_prices(ticker, period, interval)
     print(data)
+    end_time = time.time()
+    print(f"Elapsed time: {end_time - start_time}")
     pass
 
 
